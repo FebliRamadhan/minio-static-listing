9e0d0d0797bf43979400244ba1d36b2b0baf54ba
FebliRamadhan <ramadhan.febli11@gmail.com>
2022-02-08 23:36:03 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const minio = require('minio');
+const PORT = process.env.PORT;
+
+//static files
+app.use(express.static('views'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+const minioClient = new minio.Client({
+    endPoint: process.env.minio_endpoint,
+    accessKey: process.env.minio_accessKey,
+    secretKey: process.env.minio_secretKey,
+});
+
+app.get('/', (req, res) => {
+    minioClient.listBuckets((err, buckets) => {
+        if (err) {
+            res.status(400).json({
+                message: 'Something went wrong'
+            });
+        } else {
+            res.render('pages/folders', {
+                data: buckets
+            });
+        }
+    });  
+});
+
+app.get('/list', async (req, res) => {
+    let { bucket } = req.query;
+    let data = [];
+    let object = minioClient.listObjects(bucket, '', true, {});
+
+    object.on('data', obj => {
+        data.push(obj);
+    });
+
+    object.on('end', (obj) => {
+        let result = [];
+        data.map(item => {
+            let url = `${process.env.minio_url}${bucket}/${item.name}`
+            item.url = url;
+            result.push(item);
+        });
+        
+        res.render('pages/files', {
+            data: result
+        });
+    });
+    object.on('error', err => {
+        res.status(400).json({
+            message: 'Something went wrong'
+        });
+    });
+});
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
